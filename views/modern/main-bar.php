144e2c0b698aa8fecf8124f559160de3a3b7121e
Doi-Nguyen <vandoi2210@gmail.com>
2018-04-11 09:11:43 +0700

    change modern css

---
+++ views/modern/main-bar.php
@@ -0,0 +1,96 @@
+<div class="main-bar">
+    <a href="#" class="logo"><img src="" alt="online design"></a>
+    <ul class="menu-left">
+        <li class="menu-item active">
+            <span>file</span>
+            <ul class="sub-menu">
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+            </ul>
+        </li>
+        <li class="menu-item">
+            <span>Designs</span>
+            <ul class="sub-menu">
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+            </ul>
+        </li>
+
+        <li class="menu-item">
+            <span>Print</span>
+            <ul class="sub-menu">
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+            </ul>
+        </li>
+        <li class="menu-item">
+            <span>Help</span>
+            <ul class="sub-menu">
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+                <li class="sub-menu-item">
+                    <span>Import file</span>
+                    <small>(Ctrl+O)</small>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+    <ul class="menu-right">
+        <li class="menu-item"><span>Title</span></li>
+        <li class="menu-item"><span>Share</span></li>
+        <li class="menu-item"><span>Process</span></li>
+    </ul>
+</div>
