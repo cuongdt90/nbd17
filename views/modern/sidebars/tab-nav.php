2dbcb78821d210f1ad43b6ebc8536a9c88a3de8d
Doi-Nguyen <vandoi2210@gmail.com>
2018-05-19 15:15:42 +0700

    config admin

---
+++ views/modern/sidebars/tab-nav.php
@@ -0,0 +1,16 @@
+<div class="tabs-nav box-shadow-right">
+    <ul class="main-tabs" data-tab="tab-1">
+        <div id="selectedTab"></div>
+        <li class="tab layerTab active"><i class="icon-nbd icon-nbd-baseline-palette"></i><span><?php _e('Design','web-to-print-online-designer'); ?></span></li>
+        <li class="tab tab-first active"><i class="icon-nbd icon-nbd-package"></i><span><?php _e('Product','web-to-print-online-designer'); ?></span></li>
+        <li class="tab" ng-if="settings['nbdesigner_enable_text'] == 'yes'"><i class="icon-nbd icon-nbd-text" style="font-size: 21px"></i><span><?php _e('Text','web-to-print-online-designer'); ?></span></li>
+        <li class="tab" ng-if="settings['nbdesigner_enable_clipart'] == 'yes'"><i class="icon-nbd icon-nbd-sharp-star" style="font-size: 28px"></i><span><?php _e('Cliparts','web-to-print-online-designer'); ?></span></li>
+        <li class="tab" ng-if="settings['nbdesigner_enable_image'] == 'yes'"><i class="icon-nbd icon-nbd-images" style="font-size: 21px"></i><span><?php _e('Photos','web-to-print-online-designer'); ?></span></li>
+        <li class="tab"><i class="icon-nbd icon-nbd-geometrical-shapes-group"></i><span><?php _e('Elements','web-to-print-online-designer'); ?></span></li>
+        <li class="tab"><i class="icon-nbd icon-nbd-geometrical-shapes-group"></i><span><?php _e('Elements-Test','web-to-print-online-designer'); ?></span></li>
+        <li class="tab"><i class="icon-nbd icon-nbd-stack"></i><span><?php _e('Layers','web-to-print-online-designer'); ?></span></li>
+        <li class="tab tab-end" style="pointer-events: none"></li>
+    </ul>
+    <div class="keyboard-shortcuts"><i class="icon-nbd icon-nbd-info-circle nbd-tooltip-hover tooltipstered nbd-hover-shadow"></i></div>
+    <div class="nbd-sidebar-close"><i class="icon-nbd icon-nbd-clear"></i></div>
+</div>
---
+++ views/modern/toolbars/toolbar-common.php
@@ -0,0 +1,101 @@
+<div class="toolbar-common">
+    <ul class="nbd-main-menu">
+        <li class="menu-item item-color-fill">
+            <i class="icon-nbd icon-nbd-format-color-fill nbd-tooltip-hover color-fill" title="color" style="font-size: 21px"></i>
+            <div class="sub-menu" data-pos="center">
+                <div class="nbd-color-palette" style="position: relative">
+                    <h3 class="color-palette-label"><?php _e('Color document','web-to-print-online-designer'); ?></h3>
+                    <div class="working-palette">
+                        <ul class="main-color-palette">
+                            <li class="color-palette-add"></li>
+                            <li class="color-palette-item" data-color="#253702" title="#253702" style="color: red;"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                        </ul>
+                        <ul class="main-color-palette">
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                        </ul>
+                    </div>
+                    <h3 class="color-palette-label"><?php _e('Color document','web-to-print-online-designer'); ?></h3>
+                    <div class="pinned-palette">
+                        <ul class="main-color-palette">
+                            <li class="color-palette-item" data-color="#253702" title="#253702" style="color: red;"></li>
+                            <li class="color-palette-item" data-color="#253702" title="#253702" style="color: green;"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                        </ul>
+                    </div>
+                    <div class="pinned-palette">
+                        <ul class="main-color-palette">
+                            <li class="color-palette-item" data-color="#253702" title="#253702" style="color: red;"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                            <li class="color-palette-item"></li>
+                        </ul>
+                    </div>
+
+                    <div class="color-palette-popup"></div>
+
+                </div>
+            </div>
+        </li>
+        <li class="menu-item item-stack">
+            <i class="icon-nbd icon-nbd-layer-stack nbd-tooltip-hover" title="layer stack"></i>
+            <div class="sub-menu" data-pos="right">
+                <ul>
+                    <li class="sub-menu-item">
+                        <i class="icon-nbd icon-nbd-bring-to-front"></i>
+                        <span><?php _e('Bring to Front','web-to-print-online-designer'); ?></span>
+                        <span class="keyboard">Ctrl+Shift+]</span>
+                    </li>
+                    <li class="sub-menu-item">
+                        <i class="icon-nbd icon-nbd-bring-forward"></i>
+                        <span><?php _e('Bring Forward','web-to-print-online-designer'); ?></span>
+                        <span class="keyboard">Ctrl+]</span>
+                    </li>
+                    <li class="sub-menu-item">
+                        <i class="icon-nbd icon-nbd-sent-to-backward"></i>
+                        <span><?php _e('Send to Backward','web-to-print-online-designer'); ?></span>
+                        <span class="keyboard">Ctrl+[</span>
+                    </li>
+                    <li class="sub-menu-item">
+                        <i class="icon-nbd icon-nbd-send-to-back"></i>
+                        <span><?php _e('Send to Back','web-to-print-online-designer'); ?></span>
+                        <span class="keyboard">Ctrl+Shift+[</span>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="menu-item item-position">
+            <i class="icon-nbd icon-nbd-apps nbd-tooltip-hover" title="layer position"></i>
+            <div class="sub-menu" data-pos="right">
+                <ul>
+                    <i class="icon-nbd-clear nbd-close-sub-menu"></i>
+                    <li class="title">
+                        <span>Layer position</span>
+                        <i class="colse"></i>
+                    </li>
+                    <li><i class="icon-nbd icon-nbd-fomat-vertical-align-center nbd-tooltip-hover" title="Center vertical"></i></li>
+                    <li><i class="icon-nbd icon-nbd-fomat-top-left nbd-tooltip-hover rotate-90" title="Top left"></i></li>
+                    <li><i class="icon-nbd icon-nbd-fomat-top-left nbd-tooltip-hover rotate-45" title="Top center"></i></li>
+                    <li><i class="icon-nbd icon-nbd-fomat-top-left nbd-tooltip-hover" title="Top right"></i></li>
+                    <li><i class="icon-nbd icon-nbd-fomat-vertical-align-center nbd-tooltip-hover rotate90" title="Center horizontal"></i></li>
+                    <li><i class="icon-nbd icon-nbd-fomat-top-left nbd-tooltip-hover rotate-135" title="Middle left"></i></li>
+                    <li><i class="icon-nbd icon-nbd-bottom-center nbd-tooltip-hover middle-center" title="Middle center"></i></li>
+                    <li><i class="icon-nbd icon-nbd-fomat-top-left nbd-tooltip-hover rotate45" title="Middle right"></i></li>
+                    <li><i class="icon-nbd icon-nbd-info-circle nbd-tooltip-hover" title="Intro"></i></li>
+                    <li><i class="icon-nbd icon-nbd-fomat-top-left nbd-tooltip-hover rotate-180" title="Bottom left"></i></li>
+                    <li><i class="icon-nbd icon-nbd-fomat-top-left nbd-tooltip-hover rotate135" title="Bottom center"></i></li>
+                    <li><i class="icon-nbd icon-nbd-fomat-top-left nbd-tooltip-hover rotate90" title="Bottom right"></i></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</div>
