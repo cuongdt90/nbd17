63cc1f12e1d70791bd0ebafe8a94afbc2f9fa077
Doi-Nguyen <vandoi2210@gmail.com>
2018-05-25 17:21:11 +0700

    responsive toolbar

--- views/modern/toolbar.php
+++ views/modern/toolbar.php
@@ -1,6 +1,8 @@
 <div class="nbd-toolbar">
-    <?php include 'toolbars/toolbar-text.php'; ?>
-    <?php include 'toolbars/toolbar-image.php'; ?>
-    <?php include 'toolbars/toolbar-group.php'; ?>
-    <?php include 'toolbars/toolbar-common.php'; ?>
+    <div class="main-toolbar">
+        <?php include 'toolbars/toolbar-text.php'; ?>
+        <?php include 'toolbars/toolbar-image.php'; ?>
+        <?php include 'toolbars/toolbar-group.php'; ?>
+        <?php include 'toolbars/toolbar-common.php'; ?>
+    </div>
 </div>
