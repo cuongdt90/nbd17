2a59d93c60042ff79711feb3c847676b0a1ec114
Doi-Nguyen <vandoi2210@gmail.com>
2018-06-08 09:57:18 +0700

    color design

--- views/vista/sidebar/design.php
+++ views/vista/sidebar/design.php
@@ -1,7 +1,7 @@
 <div id="v-design-toolbar" class="v-tab-content active">
     <span class="v-title">Design</span>
     <div class="v-content" data-action="no">
-        <div class="v-scrollbar">
+        <div class="v-scrollbar layout">
             <div class="main-scrollbar">
                 <div class="items">
                     <div class="item">
@@ -27,6 +27,26 @@
                     </div>
                     <div class="item">
                         <img src="https://cdn.rendering.documents.cimpress.io/v1/vp/preview?instructions_uri=http%3a%2f%2fservices.vistaprint.com%2fsales%2fdocuments%2fpreviewing%2foriondocsignature.aspx%3ftemplate%3d1186570_B74%26hash%3d641ade%26language_id%3d36&amp;width=175&amp;category=gp&amp;merchant_metadata=B74">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="design-color">
+            <span class="v-title"><?php _e('Background Colour','web-to-print-online-designer'); ?></span>
+            <div class="v-scrollbar bg-color">
+                <div class="main-scrollbar">
+                    <div class="main-color">
+                        <ul class="items">
+                            <li class="item active" style="background-color: #fff"><i class="nbd-icon-vista nbd-icon-vista-done"></i></li>
+                            <li class="item" style="background-color: #000"><i class="nbd-icon-vista nbd-icon-vista-done"></i></li>
+                            <li class="item" style="background-color: #5E433A"><i class="nbd-icon-vista nbd-icon-vista-done"></i></li>
+                            <li class="item" style="background-color: #D4C89F"><i class="nbd-icon-vista nbd-icon-vista-done"></i></li>
+                            <li class="item" style="background-color: #D3CFD0"><i class="nbd-icon-vista nbd-icon-vista-done"></i></li>
+                            <li class="item" style="background-color: #FFE600"><i class="nbd-icon-vista nbd-icon-vista-done"></i></li>
+                            <li class="item" style="background-color: #398758"><i class="nbd-icon-vista nbd-icon-vista-done"></i></li>
+                            <li class="item" style="background-color: #928C3C"><i class="nbd-icon-vista nbd-icon-vista-done"></i></li>
+                            <li class="item" style="background-color: #928C3C"><i class="nbd-icon-vista nbd-icon-vista-done"></i></li>
+                        </ul>
                     </div>
                 </div>
             </div>
