daccc6b8af890de824605107f297d6a8fb401ece
TinyHust <tiny.hust.lee@gmail.com>
2018-07-06 10:18:59 +0700

    remove unesssential libs

--- includes/price/class.pricing-option.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('ABSPATH')) exit; // Exit if accessed directly  ?>
-<?php
-if(!class_exists('Nbdesigner_Printing_Option')){
-    class Nbdesigner_Printing_Option {
-        
-    }
-}
--- views/studio/tools-clipart.php
+++
@@ -1,4 +0,0 @@
-<?php if (!defined('ABSPATH')) exit; // Exit if accessed directly  ?>
-<div flex layout-align="start center" layout="row" style="width: 100%; height: 100%;" ng-show="currentActionType == 'illustrator'" class="item-config-bar">
-    illustrator
-</div>
